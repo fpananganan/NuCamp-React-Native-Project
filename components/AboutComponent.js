4903bfc87ed777ecaa9393e67b0a9189249be46c
Rico Pananganan <fpananganan@gmail.com>
2020-01-16 14:25:31 -0800

    Gestures Part 1

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'; 
 import { ScrollView, Text, FlatList } from 'react-native'; 
 import { Card, ListItem } from 'react-native-elements'; 
-import { PARTNERS } from '../shared/partners'; 
+import * as Animatable from 'react-native-animatable'; 
 import { connect } from 'react-redux'; 
 import { baseUrl } from '../shared/baseUrl'; 
 import Loading from './LoadingComponent'; 
@@ -54,26 +54,30 @@ class About extends Component {
         if (this.props.partners.errMess) {
             return (
                 <ScrollView>
-                    <Mission />
-                    <Card 
-                        title='Community Partners'>
-                            <Text>{this.props.partners.errMess}</Text>
-                    </Card>
+                    <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                        <Mission />
+                        <Card 
+                            title='Community Partners'>
+                                <Text>{this.props.partners.errMess}</Text>
+                        </Card>
+                    </Animatable.View>    
                 </ScrollView>
             ); 
         }
 
         return (
             <ScrollView>
-                <Mission />
-                <Card 
-                    title="Community Partners">
-                    <FlatList
-                        data={this.props.partners.partners}
-                        renderItem={renderPartner}
-                        keyExtractor={item=>item.id.toString()}
-                    />        
-                </Card>
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Mission />
+                    <Card 
+                        title="Community Partners">
+                        <FlatList
+                            data={this.props.partners.partners}
+                            renderItem={renderPartner}
+                            keyExtractor={item=>item.id.toString()}
+                        />        
+                    </Card>
+                </Animatable.View>    
             </ScrollView>
         ); 
     }
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react'; 
 import { Text, ScrollView } from 'react-native'; 
 import { Card } from 'react-native-elements'; 
+import * as Animatable from 'react-native-animatable'; 
 
 class Contact extends Component {
 
     render() {
         return (
             <ScrollView wrapperStyle={{margin:20}}>    
-                <Card title='Contact Information'>
-                    <Text> 
-                        1 Nucamp Way {"\n"} 
-                        Seattle, WA 98001 {"\n"}
-                        U.S.A {"\n"}
-                    </Text>
-                    <Text>
-                        Phone: 1-206-555-1234 {"\n"}
-                        Email: campsites@nucamp.co
-                    </Text>
-                </Card>
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Card title='Contact Information'>
+                        <Text> 
+                            1 Nucamp Way {"\n"} 
+                            Seattle, WA 98001 {"\n"}
+                            U.S.A {"\n"}
+                        </Text>
+                        <Text>
+                            Phone: 1-206-555-1234 {"\n"}
+                            Email: campsites@nucamp.co
+                        </Text>
+                    </Card>
+                </Animatable.View>    
             </ScrollView>
         ); 
     }
